03307170ceaab31d7cf263da032ab2299faa5166
sergey32355 <121023674+sergey32355@users.noreply.github.com>
2025-09-22 10:40:46 +0200

    Add files via upload

--- ui_MainWindow.py
+++ ui_MainWindow.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'ui_MainWindowvZNNir.ui'
+## Form generated from reading UI file 'ui_MainWindoweAPIcN.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.2
 ##
@@ -15,26 +15,42 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
+    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+    QTabWidget, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(894, 583)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.HellowWorldBtn = QPushButton(self.centralwidget)
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tabWidget.setGeometry(QRect(0, 0, 821, 511))
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.HellowWorldBtn = QPushButton(self.tab)
         self.HellowWorldBtn.setObjectName(u"HellowWorldBtn")
-        self.HellowWorldBtn.setGeometry(QRect(20, 20, 201, 111))
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(270, 60, 49, 16))
+        self.HellowWorldBtn.setGeometry(QRect(670, 30, 91, 31))
+        self.textbox = QLabel(self.tab)
+        self.textbox.setObjectName(u"textbox")
+        self.textbox.setGeometry(QRect(10, 10, 111, 16))
+        self.lineEdit = QLineEdit(self.tab)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(10, 30, 651, 22))
+        self.TrainClassifierBtn = QPushButton(self.tab)
+        self.TrainClassifierBtn.setObjectName(u"TrainClassifierBtn")
+        self.TrainClassifierBtn.setGeometry(QRect(10, 70, 91, 31))
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        self.menubar.setGeometry(QRect(0, 0, 894, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -42,12 +58,18 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
+        self.tabWidget.setCurrentIndex(0)
+
+
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.HellowWorldBtn.setText(QCoreApplication.translate("MainWindow", u"HellowWorld", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.HellowWorldBtn.setText(QCoreApplication.translate("MainWindow", u"Browser", None))
+        self.textbox.setText(QCoreApplication.translate("MainWindow", u"Path to database", None))
+        self.TrainClassifierBtn.setText(QCoreApplication.translate("MainWindow", u"Train", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Tab 1", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tab 2", None))
     # retranslateUi
 
